fd34c47916a675d78cd8375c2e483d0509e29788
Kirk Wight <kwight@kwight.ca>
2014-07-22 14:32:30 -0700

    Remove JS caching to avoid size issues when changing logos.

--- js/site-logo.js
+++ js/site-logo.js
@@ -5,17 +5,15 @@
 
 	api( 'site_logo', function( value ){
 		value.bind( function( newVal, oldVal ){
-			// Do we have any cached jQuery objects yet?
-			if ( ! $logo )
-				$logo = $( '.site-logo' );
-			if ( ! $size )
-				$size = $logo.attr( 'data-size' );
+			$logo = $( '.site-logo' );
+			$size = $logo.attr( 'data-size' );
 
 			// Let's update our preview logo.
 			if ( newVal && newVal.url ) {
 				// If the source was smaller than the size required by the theme, give the biggest we've got.
 				if ( ! newVal.sizes[ $size ] )
 					$size = 'full';
+
 				$logo.attr({
 					height: newVal.sizes[ $size ].height,
 					width: newVal.sizes[ $size ].width,
